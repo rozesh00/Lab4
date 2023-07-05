9d850f4c1813a6c8b363acaeb1fd4a93f6e368a6
Rozesh00 <rozeshgautam007@gmail.com>
2023-07-05 10:16:43 -0600

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,40 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //sum of two numbers
+        int first = 55;
+        int secon = 2;
+        int sum= first +secon;
+        //System.out.println("Sum of two numbers is:" + first + secon); THIS PRINTS 552!! CONCATENATION
+        System.out.println("Sum is: "+sum);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Dividing two numbers
+        double mydoub = 12.78d;
+        int first1 = (int) mydoub;
+        int secon1 = 5;
+        int divi= first1 /secon1;
+        int rema = first1 %secon1;
+        System.out.println("Division is: "+ divi); //Integer division 12/5 = 2
+        System.out.println("remainder is: "+ rema);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        //Write a Java program that takes two numbers as input and displays the product of two numbers.
+
+        Scanner readInput = new Scanner(System.in); //creating a scanner object
+
+        System.out.println("Enter first number");
+        int firstNum = readInput.nextInt();
+
+        System.out.println("Enter second number");
+        int secNum= readInput.nextInt();
+
+        int multi= firstNum*secNum;
+
+        System.out.println("multiplication: " + multi);
+
+
+
     }
 }
